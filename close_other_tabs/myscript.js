319f21f0b322a5cbb84a4e46b8e6801827ad95f0
ricky <erickmuchiri11@gmail.com>
2024-02-15 13:17:31 +0300

    first commit

---
+++ close_other_tabs/myscript.js
@@ -0,0 +1,14 @@
+﻿const elmClose = document.getElementById("close");
+
+elmClose.onclick = () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const currentTab = tabs[0];
+    chrome.tabs.query({ currentWindow: true }, (tabs) => {
+      tabs.map((tab) => {
+        if (tab.id != currentTab.id) {
+          chrome.tabs.remove(tab.id);
+        }
+      });
+    });
+  });
+}
---
+++ context_menus search for word on twitter/background.js
@@ -0,0 +1,14 @@
+﻿chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "Chapter 6",
+    title: "Search Twitter for \"%s\"",
+    type: "normal",
+    contexts: ["selection"]
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info) => {
+  chrome.tabs.create({
+    url: "http://twitter.com/search?q=" + encodeURIComponent(info.selectionText)
+  });
+});
